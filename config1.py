39072edb9f2054a3becc07d697f752fdb26c382c
akansha-nec <91887826+akansha-nec@users.noreply.github.com>
2021-11-01 05:29:13 +0530

    Add files via upload

---
+++ config1.py
@@ -0,0 +1,32 @@
+    client id =  A3T6AKIAFJKR45SAWS5Z
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+    
+  {
+    "Keys": [
+        {
+            "KeyArn": "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab",
+            "KeyId": "1234abcd-12ab-34cd-56ef-1234567890ab"
+        },
+        {
+            "KeyArn": "arn:aws:kms:us-west-2:111122223333:key/0987dcba-09fe-87dc-65ba-ab0987654321",
+            "KeyId": "0987dcba-09fe-87dc-65ba-ab0987654321"
+        },
+        {
+            "KeyArn": "arn:aws:kms:us-east-2:111122223333:key/1a2b3c4d-5e6f-1a2b-3c4d-5e6f1a2b3c4d",
+            "KeyId": "1a2b3c4d-5e6f-1a2b-3c4d-5e6f1a2b3c4d"
+        }
+    ]
+    
+    client id =  A3T6AKIAFJKR1245SAWS
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+1
+2
+3
+4
+5
+8
+11
+14
+16
+19
+
